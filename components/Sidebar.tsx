6b73fe776898d33f6452ca3fcce5d05b80075ca6
mai033 <145622463+mai033@users.noreply.github.com>
2024-05-13 22:03:59 -0700

    first commit

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -6,6 +6,7 @@ import { sidebarLinks } from '@/constants';
 import { usePathname } from 'next/navigation';
 import { cn } from '@/lib/utils';
 import Footer from './Footer';
+import PlaidLink from './PlaidLink';
 
 const Sidebar = ({ user }: SiderbarProps) => {
     const pathName = usePathname();
@@ -47,6 +48,7 @@ const Sidebar = ({ user }: SiderbarProps) => {
             </Link>
           );
         })}
+        <PlaidLink user={user}/>
       </nav>
       <Footer user={user} />
     </section>
